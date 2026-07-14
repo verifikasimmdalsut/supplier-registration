2cc08d5e01cc5fe0f70bd93f5a5dcad047dc5a90
verifikasimmdalsut <verifikasimmdalsut@gmail.com>
2026-07-14 09:07:04 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// Ganti dengan data project kamu
+const SUPABASE_URL = "https://akrjxgtevgwadhslkhee.supabase.co";
+
+const SUPABASE_KEY = "PASTE_PUBLISHABLE_KEY_DISINI";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
