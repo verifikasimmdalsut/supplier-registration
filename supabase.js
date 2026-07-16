89d810097a6c39999e8727f317150dd447d1963a
verifikasimmdalsut <verifikasimmdalsut@gmail.com>
2026-07-16 16:24:33 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://akrjxgtevgwadhslkhee.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_s5Fj5ZOXzXRWYc5YiODt-g_9Z5QUwoj";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+const db = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
