b10072d060a49cf17a206fb1673cac8f8f2a53c4
verifikasimmdalsut <verifikasimmdalsut@gmail.com>
2026-07-16 16:10:53 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,8 @@
-// Ganti dengan data project kamu
 const SUPABASE_URL = "https://akrjxgtevgwadhslkhee.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_s5Fj5ZOXzXRWYc5YiODt-g_9Z5QUwoj";
 
 const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
